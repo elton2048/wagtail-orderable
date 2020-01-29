05ccc2fb6ac95f54267de2b5a1e680940430495e
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2020-01-29 17:14:58 +0000

    Check for the right subclass
    If we are checking for subclasses of `wagtail.wagtailcore.models.Orderable`, then the Orderable should be imported from wagtails models

--- wagtailorderable/modeladmin/mixins.py
+++ wagtailorderable/modeladmin/mixins.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
-from wagtailorderable.models import Orderable
+from wagtail.wagtailcore.models import Orderable
 
 class OrderableMixin(object):
     """
